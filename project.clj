0a131c748c84abbcbd365586da1751225b0798d0
Kevin Prehn <kprehn01@gmail.com>
2019-02-27 20:49:48 -0600

    readding old files

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject process-text "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [
+	[org.clojure/clojure "1.8.0"] 
+	[enlive "1.1.5"]
+	[http-kit "2.3.0"]
+	[clj-http "3.9.1"]
+	[cheshire "5.8.1"]
+	[com.novemberain/monger "3.1.0"]
+  ]
+  :main ^:skip-aot process-text.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
